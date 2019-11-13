ac8c4642cd3ca15f233eeab4ed599244c42b07be
Uladzislau Bolatau <uladzislau_bolatau@epam.com>
2019-11-14 00:13:31 +0300

    #1 Typescript: create basic components and interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { UserComponent } from './core/user/user.component';
+
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { CourseCardComponent } from './components/course-card/course-card.component';
+import { SearchComponent } from './components/search/search.component';
+
+import { CoursesComponent } from './pages/courses/courses.component';
+
 @NgModule({
   declarations: [
     AppComponent,
+    BreadcrumbsComponent,
+    CourseCardComponent,
+    CoursesComponent,
+    FooterComponent,
+    HeaderComponent,
+    SearchComponent,
+    UserComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
